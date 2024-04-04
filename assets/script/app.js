b19ef1f1a187948d6b365298a6ba1e6756a30208
BoXi9252 <zhaoyanglove2022@126.com>
2024-04-04 17:13:17 -0500

    push new

--- assets/script/app.js
+++ assets/script/app.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // 如果cookie不存在，显示cookie信息弹窗
         setTimeout(function () {
             document.getElementById('cookieModal').classList.add('show');
-        }, 2000); // 页面加载后2秒显示
+        }, 1000); // 页面加载后2秒显示
     }
 });
 
@@ -37,6 +37,15 @@ function setCookie(name, value, seconds) {
     const cookieValue = name + "=" + value + "; expires=" + expiry.toGMTString() + "; path=/";
     document.cookie = cookieValue;
 }
+// Set cookie
+// function setCookie(name, value, seconds) {
+//     const expiry = new Date(new Date().getTime() + seconds * 1000);
+//     // 如果你的网站完全运行在HTTPS上，并且你需要在第三方上下文中发送cookie，则设置SameSite=None; Secure
+//     // 否则，可以选择使用SameSite=Lax来增加安全性
+//     const cookieValue = `${name}=${value}; expires=${expiry.toGMTString()}; path=/; SameSite=Lax`;
+//     document.cookie = cookieValue;
+// }
+
 
 // Get cookie
 function getCookie(name) {
@@ -50,12 +59,57 @@ function getCookie(name) {
     return null;
 }
 
+// 获取操作系统信息的函数
+function getOSInfo() {
+    let osName = "Unknown OS";
+    if (navigator.appVersion.indexOf("Win") != -1) osName = "Windows";
+    if (navigator.appVersion.indexOf("Mac") != -1) osName = "MacOS";
+    if (navigator.appVersion.indexOf("X11") != -1) osName = "UNIX";
+    if (navigator.appVersion.indexOf("Linux") != -1) osName = "Linux";
+    // console.log("Operating System:", osName); // 使用console.log输出操作系统信息
+    return osName;
+}
+
+// 获取屏幕宽度和高度的函数
+function getScreenInfo() {
+    return {
+        width: window.screen.width,
+        height: window.screen.height
+    };
+}
+
+
 // click ”Save Preferences“
 document.getElementById('savePreferences').addEventListener('click', function () {
+    // 假设用户同意保存所有信息（这里可以根据实际情况调整逻辑）
+
+    // 获取操作系统信息并保存到cookie
+    var osInfo = getOSInfo();
+    setCookie('osInfo', osInfo, 20); // 存活20秒
+
+    // 获取屏幕信息并保存到cookie
+    var screenInfo = getScreenInfo();
+    setCookie('screenWidth', screenInfo.width, 20); // 存活20秒
+    setCookie('screenHeight', screenInfo.height, 20); // 存活20秒
+
     // 隐藏settings pop up
     document.getElementById('settingsModal').style.display = 'none';
 
     // 隐藏cookie pop up
     document.getElementById('cookieModal').style.display = 'none';
-
 });
+
+
+
+
+
+
+// // click ”Save Preferences“
+// document.getElementById('savePreferences').addEventListener('click', function () {
+//     // 隐藏settings pop up
+//     document.getElementById('settingsModal').style.display = 'none';
+
+//     // 隐藏cookie pop up
+//     document.getElementById('cookieModal').style.display = 'none';
+
+// });
